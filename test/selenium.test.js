c07aa9c3ef954f7bda1ffa42156b7c4da76f9067
Škola Rajak QA Kurs Grupa 9 <skolarajak.qa@gmail.com>
2021-12-13 21:34:06 +0100

    Update tests

--- test/selenium.test.js
+++ test/selenium.test.js
@@ -16,4 +16,8 @@ describe('Maths test', function() {
     it('Test if 1 + 1 = 2', function() {
         assert.equal(MyMath.add(1, 1), 2);
     });
+
+    it('Test if 1 - 1 = 0', function() {
+        assert.equal(MyMath.sub(1, 1), 0);
+    });
 });
