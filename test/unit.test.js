1fe90abe5ff08d8cddfd580bdf4807da47d6ca2d
proofreader17 <93739368+proofreader17@users.noreply.github.com>
2021-12-14 21:31:41 +0100

    Update unit.test.js

--- test/unit.test.js
+++ test/unit.test.js
@@ -9,4 +9,12 @@ describe('Maths test', function() {
     it('Test if 1 - 1 = 0', function() {
         assert.equal(MyMath.sub(1, 1), 0);
     });
+    
+    it('Test if 1 * 1 = 1', function() {
+        assert.equal(MyMath.mul(1,1), 1);
+    });
+    
+    it('Test if 1 / 1 = 1', function() {
+        assert.equal(MyMath.div(1,1), 1);
+    });    
 });
