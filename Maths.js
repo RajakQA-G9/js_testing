29e215d9075ff69f10d3fb23aae147e6094f1e1f
proofreader17 <93739368+proofreader17@users.noreply.github.com>
2021-12-14 21:27:04 +0100

    Update Maths.js

--- Maths.js
+++ Maths.js
@@ -6,6 +6,15 @@ class Maths {
     static sub(num1, num2) {
         return num1 - num2;
     }
+    
+    static mul(num1, num2) {
+        return num1 * num2;
+    }
+    
+    static div(num1, num2) {
+        return num1 / num2;
+    }    
+ 
 }
 
 module.exports = Maths;
