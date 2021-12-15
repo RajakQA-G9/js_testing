9f495d408036667cb2114f7ed85be9bfd2c3e23a
Michek246 <93940148+Michek246@users.noreply.github.com>
2021-12-15 10:39:06 +0100

    Add files via upload

--- unit.test.js
+++ unit.test.js
@@ -9,4 +9,14 @@ describe('Maths test', function() {
     it('Test if 1 - 1 = 0', function() {
         assert.equal(MyMath.sub(1, 1), 0);
     });
+    it('Test if 1 * 2= 2', function() {
+        assert.equal(MyMath.mul(1, 2), 2);
+    });
+
+    it('Test if 1/0 = infinity', function() {
+        assert.equal(MyMath.div(1, 0), Infinity);
+    });
+    it('Test if 10/2 = 5', function() {
+        assert.equal(MyMath.div(10, 2), 5);
+    });
 });
