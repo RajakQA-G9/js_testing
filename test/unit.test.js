8b5f1f101631b30496a523158c5c9b2e06674f31
maja-n <maja.ninkovic.vg@gmail.com>
2021-12-24 17:04:25 +0100

    updated tests

--- test/unit.test.js
+++ test/unit.test.js
@@ -9,4 +9,12 @@ describe('Maths test', function() {
     it('Test if 1 - 1 = 0', function() {
         assert.equal(MyMath.sub(1, 1), 0);
     });
+
+    it('Test if 3 * 4 = 12', function() {
+        assert.equal(MyMath.mul(3, 4),12);
+    });
+
+    it('Test if 6 / 2 = 3', function() {
+        assert.equal(MyMath.div(6, 2), 3);
+    }); 
 });
