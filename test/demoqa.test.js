d1efa1cf9c8ac640e2e25c2d8eda34f9584de0dc
Škola Rajak QA Kurs Grupa 9 <skolarajak.qa@gmail.com>
2021-12-27 22:39:44 +0100

    Update demoqa tests

--- test/demoqa.test.js
+++ test/demoqa.test.js
@@ -27,11 +27,51 @@ describe('DemoQA test', function() {
 
     it('Opens Elements page', async function() {
         const elementsPage = await driver.findElement(
-            By.xpath('//h5[contains(., "Elements")]')
+            By.xpath('//h5[contains(., "Elements")]/parent::div[contains(@class, "card")]')
         );
         await elementsPage.click();
 
         expect(await driver.findElement(By.className('main-header')).getText()).to.eq('Elements');
         expect(await driver.getCurrentUrl()).to.eq('https://demoqa.com/elements');
+    });
+
+    it('Opens TextBox page, fills the form, and submits', async function() {
+        const textBoxPage = await driver.findElement(By.id('item-0'));
+        await textBoxPage.click();
+ 
+        expect(await driver.findElement(By.className('main-header')).getText()).to.eq('Text Box');
+
+        const fillUserName = 'John Doe';
+        const fillUserEmail = 'john.doe@example.org';
+        const fillCurrentAddress = 'Unknown rd. 167 apt 26';
+        const fillPermanentAddress = 'Unknown rd. 167 apt 26';
+
+        const fullName = await driver.findElement(By.id('userName'));
+        fullName.sendKeys(fillUserName);
+  
+        const email = await driver.findElement(By.id('userEmail'));
+        email.sendKeys(fillUserEmail);
+  
+        const currentAddress = await driver.findElement(By.id('currentAddress'));
+        currentAddress.sendKeys(fillCurrentAddress);
+  
+        const permanentAddress = await driver.findElement(By.id('permanentAddress'));
+        permanentAddress.sendKeys(fillPermanentAddress);
+
+        const buttonSubmit = await driver.findElement(By.id('submit'));
+        await buttonSubmit.click();
+  
+        const output = await driver.findElement(By.id('output'));
+        expect(await output.isDisplayed()).to.eq(true);
+
+        const outUserName = await output.findElement(By.id('name'));
+        const outUserEmail = await output.findElement(By.id('email'));
+        const outCurrentAddress = await output.findElement(By.id('currentAddress'));
+        const outPermanentAddress = await output.findElement(By.id('permanentAddress'));
+
+        expect(await outUserName.getText()).to.contain(fillUserName);
+        expect(await outUserEmail.getText()).to.contain(fillUserEmail);
+        expect(await outCurrentAddress.getText()).to.contain(fillCurrentAddress);
+        expect(await outPermanentAddress.getText()).to.contain(fillPermanentAddress);
     });
 });
