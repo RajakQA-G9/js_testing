f054d2c0847abd718e1e0f9a67e6b3dac2dd2da2
Škola Rajak QA Kurs Grupa 9 <skolarajak.qa@gmail.com>
2021-12-27 20:18:11 +0100

    Add demoqa test

---
+++ test/demoqa.test.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const { Builder, By, until, Key } = require('selenium-webdriver');
+const chai = require('chai');
+const expect = chai.expect;
+const chrome = require('selenium-webdriver/chrome');
+
+describe('DemoQA test', function() {
+    let driver;
+
+    before(async function() {
+        let service = new chrome.ServiceBuilder('D:\\Kurs\\chromedriver\\chromedriver.exe').build()
+        chrome.setDefaultService(service);
+
+        driver = await new Builder().forBrowser('chrome').build();
+    });
+
+    after(function() {
+        return driver.quit();
+    });
+});
