d8aad52ee7fb8550491a6336a7919691f611ab22
Škola Rajak QA Kurs Grupa 9 <skolarajak.qa@gmail.com>
2021-12-29 22:51:49 +0100

    Improve demoqa tests

--- test/demoqa.test.js
+++ test/demoqa.test.js
@@ -74,4 +74,111 @@ describe('DemoQA test', function() {
         expect(await outCurrentAddress.getText()).to.contain(fillCurrentAddress);
         expect(await outPermanentAddress.getText()).to.contain(fillPermanentAddress);
     });
+
+    it('Opens Radio Button page, and chooses', async function() {
+        const radioButton = await driver.findElement(By.id('item-2'));
+        await radioButton.click();
+
+        expect(await driver.getCurrentUrl()).to.eq('https://demoqa.com/radio-button');
+
+        const selectYes = await driver.findElement(By.xpath('//label[contains(., "Yes")]'));
+        await selectYes.click();
+
+        let message = await driver.findElement(By.className('text-success'));
+        expect(await message.getText()).to.contains('Yes');
+
+        const optionImpressive = await driver.findElement(By.xpath('//label[@for="impressiveRadio"]'));
+        await optionImpressive.click();
+
+        message = await driver.findElement(By.className('text-success'));
+        expect(await message.getText()).to.contains('Impressive');
+    });
+
+    it('Tests dynamic properties', async function() {
+        const dynamicProperties = await driver.findElement(By.id('item-8'));
+        await dynamicProperties.click();
+
+        expect(await driver.findElement(By.className('main-header')).getText()).to.eq('Dynamic Properties');
+
+        const buttonEnabledAfter = await driver.findElement(By.id('enableAfter'));
+        await driver.wait(until.elementIsEnabled(buttonEnabledAfter));
+
+        expect(await buttonEnabledAfter.isEnabled()).to.eq(true);
+
+        const buttonVisibleAfter = await driver.findElement(By.id('visibleAfter'));
+        await driver.wait(until.elementIsVisible(buttonVisibleAfter));
+
+        expect(await buttonEnabledAfter.isDisplayed()).to.eq(true);
+    });
+
+    it('Tests web tables', async function() {
+        const webTables = await driver.findElement(By.id('item-3'));
+        await webTables.click();
+
+        expect(await driver.getCurrentUrl()).to.eq('https://demoqa.com/webtables');
+
+        const edit = await driver.findElement(By.id('edit-record-1'));
+        await edit.click();
+
+        const registrationForm = await driver.findElement(By.id('registration-form-modal'));
+        await driver.wait(until.elementIsVisible(registrationForm));
+
+        expect(await registrationForm.isDisplayed()).to.eq(true);
+
+        const salary = await driver.findElement(By.id('salary'));
+        const oldSalary = await salary.getAttribute('value');
+        const newSalary = Number(oldSalary) + 1000;
+
+        await salary.sendKeys(
+            Key.chord(Key.CONTROL, 'a'),
+            Key.DELETE,
+            newSalary
+        );
+
+        const submit = await driver.findElement(By.id('submit'));
+        await submit.click();
+
+        const row = await driver.findElement(By.xpath('//div[@role="row" and contains(., "Cierra")]'));
+        const cell = await row.findElement(By.xpath('(//div[@role="gridcell"])[5]'));
+
+        expect(await cell.getText()).to.eq(newSalary.toString());
+    });
+
+    it('Test alerts', async function() {
+        await driver.get('https://demoqa.com/alerts');
+        expect(await driver.getCurrentUrl()).to.eq('https://demoqa.com/alerts');
+
+        const alertButton = await driver.findElement(By.id('alertButton'));
+        await alertButton.click();
+        await driver.wait(until.alertIsPresent());
+
+        const alert = await driver.switchTo().alert();
+        expect(await alert.getText()).to.contain('You clicked');
+
+        await alert.accept();
+
+        const alertButtonTimer = await driver.findElement(By.id('timerAlertButton'));
+        await alertButtonTimer.click();
+        await driver.wait(until.alertIsPresent());
+
+        const alertTimed = await driver.switchTo().alert();
+        expect(await alertTimed.getText()).to.contain('5 seconds');
+
+        await alertTimed.accept();
+    });
+
+    it('Tests confirmations', async function() {
+        const confirmButton = await driver.findElement(By.id('confirmButton'));
+        await confirmButton.click();
+        await driver.wait(until.alertIsPresent());
+
+        const confirmation = await driver.switchTo().alert();
+        expect(await confirmation.getText()).to.contain('Do you confirm');
+
+        await confirmation.dismiss();
+
+        const confirmResult = await driver.findElement(By.id('confirmResult'));
+        expect(await confirmResult.isDisplayed()).to.eq(true);
+        expect(await confirmResult.getText()).to.contain('Cancel');
+    });
 });
